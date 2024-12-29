0639291a210b627ce57b3554f0a8e48e106437f2
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2024-12-29 10:32:42 +0900

    Lexerクラスの実装

---
+++ db/parse/lexer.py
@@ -0,0 +1,141 @@
+import re
+from typing import Optional
+
+
+class Lexer:
+    def __init__(self, sql: str) -> None:
+        """SQL文を解析するための字句解析器"""
+        self.keywords = {
+            "select",
+            "from",
+            "where",
+            "and",
+            "or",
+            "not",
+            "insert",
+            "into",
+            "values",
+            "delete",
+            "update",
+            "set",
+            "create",
+            "table",
+            "view",
+            "drop",
+            "index",
+            "on",
+            "primary",
+            "key",
+            "int",
+            "char",
+            "varchar",
+            "text",
+            "date",
+            "time",
+            "datetime",
+            "null",
+            "is",
+            "like",
+            "order",
+            "by",
+            "as",
+            "inner",
+            "left",
+            "right",
+            "outer",
+            "join",
+            "union",
+            "all",
+            "exists",
+            "case",
+            "when",
+            "then",
+            "else",
+            "end",
+            "in",
+            "between",
+            "exists",
+            "limit",
+            "offset",
+        }
+        self.tokens = self._tokenize(sql)
+        self.current_token: Optional[str] = None
+        self.next_token()
+
+    def _tokenize(self, sql: str) -> list[str]:
+        """SQL文をトークンに分割"""
+        token_pattern = r"[a-zA-Z_][a-zA-Z_0-9]*|'[^']*'|\d+|[=,()<>*+-/]|."
+        tokens = re.findall(token_pattern, sql.lower())
+
+        return [token for token in tokens if not token.isspace()]
+
+    def match_delimiter(self, delimiter: str) -> bool:
+        """指定されたデリミタと現在のトークンが一致するかどうかを返す"""
+
+        return self.current_token == delimiter.lower() or (delimiter == "*" and self.current_token == delimiter)
+
+    def match_int_constant(self) -> bool:
+        """現在のトークンが整数定数と一致するかどうかを返す"""
+        if self.current_token is None:
+            return False
+
+        return self.current_token.isdigit()
+
+    def match_string_constant(self, keyword: str) -> bool:
+        """現在のトークンが文字列定数と一致するかどうかを返す"""
+        return self.current_token == keyword
+
+    def match_id(self) -> bool:
+        """現在のトークンが識別子かどうかを返す"""
+        if self.current_token is None:
+            return False
+
+        return self.current_token.isidentifier() and self.current_token not in self.keywords
+
+    def eat_delimiter(self, delimiter: str) -> None:
+        """指定されたデリミタを消費"""
+        if not self.match_delimiter(delimiter):
+            raise SyntaxError(f"Expected {delimiter}, but not found {self.current_token}")
+
+        self.next_token()
+
+    def eat_int_constant(self) -> str | None:
+        """整数定数を消費"""
+        if not self.match_int_constant():
+            raise SyntaxError(f"Expected integer constant, but not found {self.current_token}")
+
+        value = self.current_token
+        self.next_token()
+        return value
+
+    def eat_string_constant(self, keyword: str) -> str | None:
+        """文字列定数を消費"""
+        if not self.match_string_constant(keyword):
+            raise SyntaxError(f"Expected string constant, but not found {self.current_token}")
+
+        value = self.current_token
+        self.next_token()
+        return value
+
+    def eat_keyword(self, keyword: str) -> None:
+        """指定されたキーワードを消費"""
+        if self.current_token != keyword.lower():
+            raise SyntaxError(f"Expected {keyword}, but not found {self.current_token}")
+
+        self.next_token()
+
+    def eat_id(self) -> str | None:
+        """識別子を消費"""
+        if not self.match_id():
+            raise SyntaxError(f"Expected identifier, but not found {self.current_token}")
+
+        value = self.current_token
+        self.next_token()
+        return value
+
+    def next_token(self) -> None:
+        """次のトークンに進む"""
+        try:
+            self.current_token = self.tokens.pop(0)
+        except IndexError:
+            self.current_token = None
---
+++ test/parse/test_lexer.py
@@ -0,0 +1,77 @@
+from db.parse.lexer import Lexer
+
+
+def test_デリミタが一致することを確認する():
+    sql = "SELECT * FROM table;"
+    lexer = Lexer(sql)
+
+    assert lexer.match_delimiter("SELECT") is True
+    lexer.next_token()
+    assert lexer.match_delimiter("*") is True
+    lexer.next_token()
+    assert lexer.match_delimiter("FROM") is True
+
+
+def test_整数定数が一致することを確認する():
+    sql = "SELECT 123 FROM table;"
+    lexer = Lexer(sql)
+
+    lexer.next_token()
+    assert lexer.match_int_constant() is True
+
+
+def test_文字列定数が一致することを確認する():
+    sql = "SELECT 'value' FROM table;"
+    lexer = Lexer(sql)
+
+    lexer.next_token()
+    assert lexer.match_string_constant("'value'") is True
+
+
+def test_識別子が一致することを確認する():
+    sql = "SELECT Column_name FROM table;"
+    lexer = Lexer(sql)
+
+    lexer.next_token()
+    print("lexer★", vars(lexer))
+    assert lexer.match_id() is True
+
+
+def test_整数定数を消費できることを確認する():
+    sql = "SELECT 123 FROM table;"
+    lexer = Lexer(sql)
+
+    lexer.next_token()
+    assert lexer.eat_int_constant() == "123"
+
+
+def test_文字列定数を消費できることを確認する():
+    sql = "SELECT 'value' FROM table;"
+    lexer = Lexer(sql)
+
+    lexer.next_token()
+    assert lexer.eat_string_constant("'value'") == "'value'"
+
+
+def test_識別子を消費できることを確認する():
+    sql = "SELECT Column_name FROM table;"
+    lexer = Lexer(sql)
+
+    lexer.next_token()
+    assert lexer.eat_id() == "column_name"
+
+
+def test_キーワードを消費できることを確認する():
+    sql = "SELECT * FROM table;"
+    lexer = Lexer(sql)
+
+    lexer.eat_keyword("SELECT")
+    assert lexer.current_token == "*"
+    lexer.eat_keyword("*")
+    assert lexer.current_token == "from"
+    lexer.eat_keyword("FROM")
+    assert lexer.current_token == "table"
+    lexer.eat_keyword("table")
+    assert lexer.current_token == ";"
+    lexer.eat_delimiter(";")
+    assert lexer.current_token is None
