558d983ccd40d15a0e19ac2e137c167e4ce97028
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2024-12-29 19:12:50 +0900

    Plan抽象基底クラスの実装

--- db/plan/plan.py
+++ db/plan/plan.py
@@ -1,8 +1,33 @@
-class Plan:
+import abc
+from abc import ABC
+
+from db.record.schema import Schema
+
+
+class Plan(ABC):
 
     def __init__(self) -> None:
         pass
 
+    @abc.abstractmethod
+    def open(self) -> None:
+        """計画の初期化"""
+        pass
+
+    @abc.abstractmethod
+    def block_accessed(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    def record_output(self) -> int:
+        pass
+
+    @abc.abstractmethod
     def distinct_values(self, field_name: str) -> int:
         """フィールドの値の種類数を返す"""
-        return 1
+        pass
+
+    @abc.abstractmethod
+    def schema(self) -> Schema:
+        """計画のスキーマを返す"""
+        pass
