3b2bf58501d876602982243dafcd0379bd2ed6fb
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2024-12-30 11:26:42 +0900

    passs lint

--- db/plan/basic_query_planner.py
+++ db/plan/basic_query_planner.py
@@ -5,7 +5,10 @@ from db.metadata.metadata_manager import MetadataManager
 from db.parse.parser import Parser
 from db.parse.query_data import QueryData
 from db.plan.plan import Plan
+from db.plan.product_plan import ProductPlan
+from db.plan.project_plan import ProjectPlan
 from db.plan.query_planner import QueryPlanner
+from db.plan.select_plan import SelectPlan
 from db.plan.table_plan import TablePlan
 from db.transaction.transaction import Transaction
 
@@ -17,6 +20,7 @@ class BasicQueryPlanner(QueryPlanner, ABC):
 
     def create_plan(self, query_data: QueryData, transaction: Transaction) -> Plan:
 
+        # Step 1: Create a plan for each mentioned table or view.
         plans: List[Plan] = []
         for table_name in query_data.tables:
             view_def = self.metadata_manager.get_view_definition(table_name, transaction)
@@ -27,5 +31,19 @@ class BasicQueryPlanner(QueryPlanner, ABC):
             else:
                 plans.append(TablePlan(transaction, table_name, self.metadata_manager))
 
-        # TODO: Implement the rest of the method
-        return Plan()
+        # Step 2: Create the product of all table plans.
+
+        plan = plans.pop(0)
+
+        for next_plan in plans:
+            plan = ProductPlan(plan, next_plan)
+
+        # step 3: Add a selection plan for the predicate.
+
+        plan = SelectPlan(plan, query_data.get_predicate())
+
+        # Step 4: Project on the field names.
+
+        plan = ProjectPlan(plan, query_data.get_fields())
+
+        return plan
