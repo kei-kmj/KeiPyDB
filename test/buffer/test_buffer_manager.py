5b79870c874810deb33e5819553afa37229b56ba
kei-kmj <kei-kmj@users.noreply.github.com>
2025-05-19 22:01:06 +0900

    add buffer manager test

--- test/buffer/test_buffer_manager.py
+++ test/buffer/test_buffer_manager.py
@@ -1,14 +1,11 @@
 from unittest.mock import Mock
-
-import pytest
-
-from db.buffer.buffer_manager import BufferAbortException, BufferManager
+from db.buffer.buffer_manager import BufferManager
 from db.file.block_id import BlockID
 from db.file.file_manager import FileManager
 from db.log.log_manager import LogManager
 
 
-def test_利用可能なバッファの数を取得できる():
+def test_get_number_of_available_buffers():
     file_manager = Mock(spec=FileManager)
     file_manager.block_size = 1024
     log_manager = Mock(spec=LogManager)
@@ -17,7 +14,7 @@ def test_利用可能なバッファの数を取得できる():
     assert buffer_manager.available() == 3
 
 
-def test_バッファをフラッシュできる():
+def test_flush_modified_buffer():
     file_manager = Mock(spec=FileManager)
     file_manager.block_size = 1024
     log_manager = Mock(spec=LogManager)
@@ -34,7 +31,7 @@ def test_バッファをフラッシュできる():
     file_manager.write.assert_called_once_with(block, buffer.contents)
 
 
-def test_ブロックをピンできる():
+def test_pin_block():
     file_manager = Mock(spec=FileManager)
     file_manager.block_size = 1024
     log_manager = Mock(spec=LogManager)
@@ -48,7 +45,7 @@ def test_ブロックをピンできる():
     assert buffer.pins == 1
 
 
-def test_ブロックをアンピンできる():
+def test_unpin_block():
     file_manager = Mock(spec=FileManager)
     file_manager.block_size = 1024
     log_manager = Mock(spec=LogManager)
@@ -63,19 +60,7 @@ def test_ブロックをアンピンできる():
     assert buffer_manager.available() == 3
 
 
-@pytest.mark.skip
-def test_タイムアウトで例外が発生する():
-    file_manager = Mock(spec=FileManager)
-    log_manager = Mock(spec=LogManager)
-    buffer_manager = BufferManager(file_manager, log_manager, num_buffers=0)
-
-    block = BlockID("testfile", 1)
-
-    with pytest.raises(BufferAbortException):
-        buffer_manager.pin(block)
-
-
-def test_利用可能なバッファを探せる():
+def test_fined_existing_buffer():
     file_manager = Mock(spec=FileManager)
     file_manager.block_size = 1024
     log_manager = Mock(spec=LogManager)
